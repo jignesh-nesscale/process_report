40c60d81db62597a77239d3f8fc0fd73ebe7284e
jigneshchuadhary757 <jigneshchaudhary757@gmail.com>
2024-10-10 19:17:23 +0530

    process statement of acounts

---
+++ frappe_ats/frappe_ats/doctype/job_description/job_description.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, jignesh  and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Job Description", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ frappe_ats/frappe_ats/doctype/job_description/job_description.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, jignesh  and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class JobDescription(Document):
+	pass
